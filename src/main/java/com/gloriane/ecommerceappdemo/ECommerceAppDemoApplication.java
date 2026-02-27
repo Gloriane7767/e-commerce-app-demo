8f4ab4ad118e45d769acba23bcaea06ed47394c8
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-02-27 08:59:25 +0100

    Initial commit

---
+++ src/main/java/com/gloriane/ecommerceappdemo/ECommerceAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gloriane.ecommerceappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gloriane/ecommerceappdemo/ECommerceAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gloriane.ecommerceappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
